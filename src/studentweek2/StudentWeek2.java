4229f3ee6298cafa3d3379ad25f5371c4a637ae6
Albalkhm <54959659+Albalkhm@users.noreply.github.com>
2020-01-17 12:23:20 -0500

    initial commit for new students code

---
+++ src/studentweek2/StudentWeek2.java
@@ -0,0 +1,15 @@
+package studentweek2;
+
+/**
+ * This class +++Insert Description Here+++
+ * @author Mohammad Al Balkhi 
+ */
+public class StudentWeek2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
