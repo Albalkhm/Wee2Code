22d275767413c6d46de473377ae194aee3ba8a28
Albalkhm <54959659+Albalkhm@users.noreply.github.com>
2020-03-04 21:04:57 -0500

    Revert "This is the group assingment"
    This reverts commit 07900881ff547489787037a48b3782caa5441b42.

--- src/studentweek2/StudentWeek2.java
+++ src/studentweek2/StudentWeek2.java
@@ -10,13 +10,6 @@ public class StudentWeek2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Student student1 = new Student ();
-        Student student2 = new Student ();
-        Student student3 = new Student ();
-        
-        System.out.println(student1 == student2);
-        System.out.println(student1 == student3);
     }
 
 }
